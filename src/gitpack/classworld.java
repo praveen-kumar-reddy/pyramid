635d9baee6f3df631301b055b537bdae448cf528
praveen-kumar-reddy <118421197+praveen-kumar-reddy@users.noreply.github.com>
2023-09-13 10:46:55 +0530

    class

---
+++ src/gitpack/classworld.java
@@ -0,0 +1,13 @@
+package gitpack;
+
+public class classworld {
+	public static void test() {
+		System.out.println("the test has been fast");
+	}
+
+	public static void main(String[] args) {
+		 
+classworld.test();
+	}
+
+}
